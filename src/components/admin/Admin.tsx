cc02990ac72ac4cec8d052743efdf91a7bce4799
ruben.verster <ruben.verster@1-grid.com>
2021-09-20 04:51:51 +0200

    logic added to see if user already logged in

--- src/components/admin/Admin.tsx
+++ src/components/admin/Admin.tsx
@@ -1,9 +1,23 @@
 import Login from './Login';
 import Dashboard from './dashboard/Dashboard';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 const Admin = () => {
-  const [userLoggedIn, setUserLoggedIn] = useState<boolean>(true);
+  const [userLoggedIn, setUserLoggedIn] = useState<boolean>(false);
+  const checkIfUserLoggedIn = () => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUserLoggedIn(true);
+      } else {
+        setUserLoggedIn(false);
+      }
+    });
+  };
+  useEffect(() => {
+    checkIfUserLoggedIn();
+  }, []);
 
   return (
     <div className="">
